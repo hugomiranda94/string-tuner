c018ea4165fb16320b798be720af1b881d70a9c6
Hugo Miranda <hughmiranda94@gmail.com>
2026-03-01 00:07:48 -0700

    URL Regex fixed

--- src/regex.ts
+++ src/regex.ts
@@ -20,7 +20,7 @@ export const REGEX_SLUGIFY_TRIM_DASHES = /^-+|-+$/g;
 // STRING VALIDATION PATTERNS
 export const REGEX_EMAIL_DEFAULT = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const REGEX_PHONE_DEFAULT = /^[\+]?[(]?[0-9]{1,4}[)]?[-\s\.]?[(]?[0-9]{1,4}[)]?[-\s\.]?[0-9]{1,4}[-\s\.]?[0-9]{1,4}$/;
-export const REGEX_URL_DEFAULT = /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/;
+export const REGEX_URL_DEFAULT = /^((https?:\/\/|ftp:\/\/)?([a-zA-Z0-9-]+[\.]{1})?(([a-zA-Z0-9-]+)([\.]{1}[^\s\.]{2,})+|localhost(:[0-9]{1,5})?)|data:[a-zA-Z0-9-]+)$/;
 
 // STRING EXTRACTION PATTERNS
 export const REGEX_INITIALS_WHITESPACE = /\s+/;
